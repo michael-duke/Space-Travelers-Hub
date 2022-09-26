21519ac27da27e6dd03f5df5e6b666c208651ec7
PraisesPJMT <praisesmusa@outlook.com>
2022-09-26 20:45:35 +0100

    Implement dynamic rendering of rocket lists using the RocketList component

--- src/components/RocketContainer.js
+++ src/components/RocketContainer.js
@@ -3,20 +3,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getRockets, allRockets } from '../redux/rockets/rocketsSlice';
 import RocketList from './RocketList';
 
-function RocketContainer() {
+const RocketContainer = () => {
   const rockets = useSelector(allRockets);
   const dispatch = useDispatch();
-
-  console.log(rockets);
 
   useEffect(() => {
     dispatch(getRockets());
   }, [dispatch]);
   return (
     <>
-      <RocketList />
+      { rockets.map((rocket) => (
+        <RocketList
+          key={rocket.rocketId}
+          rocketImage={rocket.rocketImage}
+          rocketName={rocket.rocketName}
+          rocketDescription={rocket.rocketDescription}
+        />
+      )) }
     </>
   );
-}
+};
 
 export default RocketContainer;
