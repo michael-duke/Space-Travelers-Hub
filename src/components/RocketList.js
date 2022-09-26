aa8583804fe897319f6dd30d7c57a5ebaa457487
PraisesPJMT <praisesmusa@outlook.com>
2022-09-26 20:44:07 +0100

    Draft rocket structure for render

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Button } from '@material-tailwind/react';
+import PropTypes from 'prop-types';
 
-const RocketList = () => (
-  <>
-    <h1>From Rocket List</h1>
-  </>
+const RocketList = ({
+  rocketImage, rocketName, rocketDescription,
+}) => (
+  <div>
+    <img
+      className=""
+      src={rocketImage}
+      alt={rocketName}
+    />
+    <div className="">
+      <h3>{ rocketName }</h3>
+      <p>{ rocketDescription }</p>
+      <Button
+        className=""
+      >
+        Reserve Rockets
+      </Button>
+    </div>
+  </div>
 );
+
+RocketList.propTypes = {
+  rocketImage: PropTypes.string.isRequired,
+  rocketName: PropTypes.string.isRequired,
+  rocketDescription: PropTypes.string.isRequired,
+};
 
 export default RocketList;
