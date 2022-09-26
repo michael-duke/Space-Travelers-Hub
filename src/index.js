815b00009153736f035107a5b10360ec4f98c662
michael-duke <michael.21duke@gmail.com>
2022-09-26 15:39:30 +0300

    Incorparate Tailwind in the React App and use ThemeProvider wrapper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@material-tailwind/react';
 import App from './App';
+import 'tw-elements';
 import './index.css';
 
 const container = document.getElementById('root');
@@ -8,6 +10,8 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
