e2ca3ef992159ef3fc5c62eae3584d0986c5c8e8
michael-duke <michael.21duke@gmail.com>
2022-09-26 14:46:46 +0300

    Make the store available for Parent component and respective Child components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
