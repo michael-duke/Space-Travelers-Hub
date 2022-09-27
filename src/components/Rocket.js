ad9aaf94be8c5716c1cdba67215b5ad727ab7d09
PraisesPJMT <praisesmusa@outlook.com>
2022-09-27 15:21:29 +0100

    Add event for booking rocket

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -3,7 +3,7 @@ import { Button } from '@material-tailwind/react';
 import PropTypes from 'prop-types';
 
 const Rocket = ({
-  rocketId, rocketImage, rocketName, rocketDescription,
+  rocketId, rocketImage, rocketName, rocketDescription, rocketReservation, handleBooking,
 }) => (
   <div className="flex gap-6">
     <img
@@ -14,8 +14,13 @@ const Rocket = ({
     <div className="text-left">
       <h3 className="font-bold">{ rocketName }</h3>
       <p className="my-2">{ rocketDescription }</p>
-      <Button id={rocketId}>
-        Reserve Rockets
+      <Button
+        id={rocketId}
+        onClick={() => handleBooking(rocketId)}
+        className={rocketReservation ? 'bg-blue-700 px-2.5 py-2.5 rounded-md text-gray-800 border-2 border-gray-800 bg-white hover:text-white hover:border-none hover:bg-gray-800'
+          : 'bg-blue-700 px-2.5 py-2.5 rounded-md hover:bg-white hover:text-blue-700 hover:border-2 hover:border-blue-700'}
+      >
+        { rocketReservation ? 'Cancel Reservation' : 'Reserve Rockets' }
       </Button>
     </div>
   </div>
@@ -26,6 +31,8 @@ Rocket.propTypes = {
   rocketImage: PropTypes.string.isRequired,
   rocketName: PropTypes.string.isRequired,
   rocketDescription: PropTypes.string.isRequired,
+  rocketReservation: PropTypes.bool.isRequired,
+  handleBooking: PropTypes.func.isRequired,
 };
 
 export default Rocket;
