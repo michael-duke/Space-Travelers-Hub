40e0a03224e10eff5b222e7e2a4be34712884671
PraisesPJMT <praisesmusa@outlook.com>
2022-09-28 10:38:33 +0100

    Made tests for Rockets and Profile component
    - Add test for Rocket rendering
    - Add test for Profile rendering
    - Made sure the headings 'My Missions' & 'My Rockets' are in the Profile page

--- src/__tests__/Rockets.test.js
+++ src/__tests__/Rockets.test.js
@@ -3,19 +3,19 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../redux/configureStore';
-import MissionsContainer from '../components/MissionsContainer';
+import RocketsContainer from '../components/RocketsContainer';
 
-describe('Mission Container component', () => {
-  test('Renders Missions correctly', () => {
-    const missions = render(
+describe('Rockets Container component', () => {
+  test('Renders Rockets correctly', () => {
+    const rockets = render(
       <React.StrictMode>
         <Provider store={store}>
           <Router>
-            <MissionsContainer />
+            <RocketsContainer />
           </Router>
         </Provider>
       </React.StrictMode>,
     );
-    expect(missions).toMatchSnapshot();
+    expect(rockets).toMatchSnapshot();
   });
 });
